96f8843177885ab40aa16338b6ab84f36e999df5
chelovrk232132 <stepapopoffff1@gmail.com>
2024-06-21 12:56:36 +0300

    Update BuiscRIpthub.lua

--- BuiscRIpthub.lua
+++ BuiscRIpthub.lua
@@ -35,10 +35,10 @@ local colors = {
 local Window = Library.CreateLib("ScriptHub", "RJTheme3")
 
 -- Секция
-local Tab = Window:NewTab("келвин вруби сервак")
+local Tab = Window:NewTab("Скрипты")
 
 -- Подсекция
-local Section = Tab:NewSection("посмотри вниз")
+local Section = Tab:NewSection("ScriptHub")
 
 -- Заголовок
 Section:NewLabel("Снизу скрипты")
@@ -91,20 +91,9 @@ end)
 Section:NewButton("Арсенал", "ButtonInfo", function()
          loadstring(game:HttpGet('https://raw.githubusercontent.com/wuhaz/arsenal/main/main.lua'))()
 end)
-local Tab = Window:NewTab("хуйня всякая")
-
-local Section = Tab:NewSection("снизу не скрипт хаб")
--- Кнопка
-Section:NewButton("Infinity Yield", "ButtonInfo", function()
-         loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end)
--- Кнопка
-Section:NewButton("флай", "ButtonInfo", function()
-         loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-end)
 local Tab = Window:NewTab("Обычные функции")
 
-local Section = Tab:NewSection("посмотри вниз")
+local Section = Tab:NewSection("ScriptHub")
 -- Кнопка
 Section:NewButton("Infinity Yield", "ButtonInfo", function()
          loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
